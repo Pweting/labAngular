fcb2c755e53b30e1e782c67f6778c29e45a52778
Pweting <arnaud.parrot@outlook.fr>
2019-02-08 17:12:06 +0100

    Classe vin + bootstrap

---
+++ src/app/modele/vin.ts
@@ -0,0 +1,12 @@
+export class Vin {
+    constructor(private id: number,
+    private nom: string,
+    private annee: number,
+    private vieillissement: number,
+    private type: string,
+    private region: string,
+    private commentaire: string,
+    private plat: string) {
+    }
+
+}
